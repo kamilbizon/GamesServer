65cfc7704d0e812f28948c9c404ed4205c2d9dd6
kamilbizon <kamilbizon@outlook.com>
2018-04-27 13:19:35 +0100

    More clear moreless messeges

--- client/morelessClient/MoreLessClient.py
+++ client/morelessClient/MoreLessClient.py
@@ -6,6 +6,20 @@ from time import sleep
 
 class MoreLessClient:
 
+    MESSAGES_FROM_SERVER = {'Welcome': 'WE',
+                          'AskMinRange': 'AMI',
+                          'GetMinRange': 'GMI',
+                          'WrongMinRange': 'WMI',
+                          'AskMaxRange': 'AMX',
+                          'GetMaxRange': 'GMX',
+                          'WrongMaxRange': 'WMX',
+                          'AskPlayerGuess': 'APG',
+                          'GetGuess': 'GG',
+                          'WrongGuess': 'WG',
+                          'Less': 'LS',
+                          'More': 'MR',
+                          'CongratulateWin': 'CW'}
+
     PAUSE_TIME = 0.1
 
     def __init__(self, sock):
@@ -43,41 +57,41 @@ class MoreLessClient:
         header = message.get_header()
         data = message.get_body()
 
-        if header == 'WE':
+        if header == self.MESSAGES_FROM_SERVER['Welcome']:
             self._outputCon.welcome()
-        elif header == 'AMI':
+        elif header == self.MESSAGES_FROM_SERVER['AskMinRange']:
             self._outputCon.ask_min_range()
-        elif header == 'GMI':
+        elif header == self.MESSAGES_FROM_SERVER['GetMinRange']:
             min_range = self._inputCon.get_min_range()
             message = OnlineMessage('', min_range)
             self.send(message)
-        elif header == 'WMI':
+        elif header == self.MESSAGES_FROM_SERVER['WrongMinRange']:
             self._outputCon.wrong_min_range()
-        elif header == 'AMX':
+        elif header == self.MESSAGES_FROM_SERVER['AskMaxRange']:
             self._outputCon.ask_max_range()
-        elif header == 'GMX':
+        elif header == self.MESSAGES_FROM_SERVER['GetMaxRange']:
             min_range = data
             max_range = self._inputCon.get_max_range(min_range)
             message = OnlineMessage('', max_range)
             self.send(message)
-        elif header == 'WMX':
+        elif header == self.MESSAGES_FROM_SERVER['WrongMaxRange']:
             min_range = data
             self._outputCon.wrong_max_range(min_range)
-        elif header == 'APG':
+        elif header == self.MESSAGES_FROM_SERVER['AskPlayerGuess']:
             self._outputCon.ask_player_guess()
-        elif header == 'GG':
+        elif header == self.MESSAGES_FROM_SERVER['GetGuess']:
             min_range, max_range = data[0], data[1]
             guess = self._inputCon.get_guess(min_range, max_range)
             message = OnlineMessage('', guess)
             self.send(message)
-        elif header == 'WG':
+        elif header == self.MESSAGES_FROM_SERVER['WrongGuess']:
             min_range, max_range = data[0], data[1]
             self._outputCon.wrong_guess(min_range, max_range)
-        elif header == 'LS':
+        elif header == self.MESSAGES_FROM_SERVER['Less']:
             self._outputCon.less()
-        elif header == 'MR':
+        elif header == self.MESSAGES_FROM_SERVER['More']:
             self._outputCon.more()
-        elif header == 'CW':
+        elif header == self.MESSAGES_FROM_SERVER['CongratulateWin']:
             self._outputCon.congratulate_win()
             self.sock.close()
             exit()
