1a61e0e164f961489422166091fbd086885de2b4
kamilbizon <kamilbizon@outlook.com>
2018-05-11 22:33:58 +0100

    test fsm connect first player works

--- Tests/TestServerTransitionFSM.py
+++ Tests/TestServerTransitionFSM.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import Mock
 from server.ServerTransitionFSM import ServerTransitionFSM, Event, State
 from client.connectClient.ConnectServerClient import ConnectServerClient
 from server.Server import Server
@@ -7,14 +7,12 @@ from server.Server import Server
 
 class TestServerTransitionFSM(unittest.TestCase):
 
-    # @patch(Server.connect_first_player)
     def test_handle_event_call_connect_first_player_for_start_server_state_start_play_event(self):
         server = Server()
+        server.connect_first_player = Mock()
+
         fsm = ServerTransitionFSM(server)
 
-        server.connect_first_player = MagicMock()
         fsm.handle_event(Event.START_PLAY)
-        client = ConnectServerClient()
-        client.connect_server()
         self.assertTrue(fsm.state == State.CONNECT_FIRST_PLAYER)
         server.connect_first_player.assert_called_once()
