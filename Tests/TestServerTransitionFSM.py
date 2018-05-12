089cb2785f8ac902c5a51bf27a2362a1c4de0840
kamilbizon <kamilbizon@outlook.com>
2018-05-12 16:34:55 +0100

    test fsm

--- Tests/TestServerTransitionFSM.py
+++ Tests/TestServerTransitionFSM.py
@@ -28,3 +28,53 @@ class TestServerTransitionFSM(unittest.TestCase):
         fsm.handle_event(Event.PLAYER_CONNECTED)
         self.assertTrue(fsm.state == State.ASK_PLAYER_GAME)
         self.server.ask_game.assert_called()
+
+    def test_handle_event_call_start_more_less_game_for_ask_player_game_state_player_choose_more_less_event(self):
+        self.server.start_more_less_game = Mock()
+
+        fsm = ServerTransitionFSM(self.server)
+        fsm.state = State.ASK_PLAYER_GAME
+
+        fsm.handle_event(Event.PLAYER_CHOOSE_MORE_LESS)
+        self.assertTrue(fsm.state == State.PLAY_MORE_LESS)
+        self.server.start_more_less_game.assert_called()
+
+    def test_handle_event_call_connect_second_player_for_ask_player_game_state_player_choose_tic_tac_toe_event(self):
+        self.server.connect_second_player = Mock()
+
+        fsm = ServerTransitionFSM(self.server)
+        fsm.state = State.ASK_PLAYER_GAME
+
+        fsm.handle_event(Event.PLAYER_CHOOSE_TIC_TAC_TOE)
+        self.assertTrue(fsm.state == State.CONNECT_SECOND_PLAYER)
+        self.server.connect_second_player.assert_called()
+
+    def test_handle_event_call_start_tic_tac_toe_game_for_connect_second_player_state_player_connected_event(self):
+        self.server.start_tic_tac_toe_game = Mock()
+
+        fsm = ServerTransitionFSM(self.server)
+        fsm.state = State.CONNECT_SECOND_PLAYER
+
+        fsm.handle_event(Event.PLAYER_CONNECTED)
+        self.assertTrue(fsm.state == State.PLAY_TIC_TAC_TOE)
+        self.server.start_tic_tac_toe_game.assert_called()
+
+    def test_handle_event_call_disconnect_players_for_play_more_less_state_game_finished_event(self):
+        self.server.disconnect_players = Mock()
+
+        fsm = ServerTransitionFSM(self.server)
+        fsm.state = State.PLAY_MORE_LESS
+
+        fsm.handle_event(Event.GAME_FINISHED)
+        self.assertTrue(fsm.state == State.START_SERVER)
+        self.server.disconnect_players.assert_called()
+
+    def test_handle_event_call_disconnect_players_for_play_tic_tac_toe_state_game_finished_event(self):
+        self.server.disconnect_players = Mock()
+
+        fsm = ServerTransitionFSM(self.server)
+        fsm.state = State.PLAY_TIC_TAC_TOE
+
+        fsm.handle_event(Event.GAME_FINISHED)
+        self.assertTrue(fsm.state == State.START_SERVER)
+        self.server.disconnect_players.assert_called()
