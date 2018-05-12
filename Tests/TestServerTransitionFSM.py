0d74e42286962be48afd77bf6407021a0417338a
kamilbizon <kamilbizon@outlook.com>
2018-05-12 01:31:37 +0100

    test fsm ask game works

--- Tests/TestServerTransitionFSM.py
+++ Tests/TestServerTransitionFSM.py
@@ -7,26 +7,24 @@ from server.Server import Server
 
 class TestServerTransitionFSM(unittest.TestCase):
 
+    server = Server()
+
+
     def test_handle_event_call_connect_first_player_for_start_server_state_start_play_event(self):
-        server = Server()
-        server.connect_first_player = Mock()
+        self.server.connect_first_player = Mock()
 
-        fsm = ServerTransitionFSM(server)
-
-        # self.fsm.transitions[0].action = Mock()
+        fsm = ServerTransitionFSM(self.server)
 
         fsm.handle_event(Event.START_PLAY)
         self.assertTrue(fsm.state == State.CONNECT_FIRST_PLAYER)
-        server.connect_first_player.assert_called()
-        # self.fsm.transitions[0].action.assert_called_once()
+        self.server.connect_first_player.assert_called()
 
     def test_handle_event_call_ask_game_for_connect_first_player_state_player_connected_event(self):
-        server = Server()
-        server.ask_game = Mock()
+        self.server.ask_game = Mock()
 
-        fsm = ServerTransitionFSM(server)
+        fsm = ServerTransitionFSM(self.server)
         fsm.state = State.CONNECT_FIRST_PLAYER
 
         fsm.handle_event(Event.PLAYER_CONNECTED)
         self.assertTrue(fsm.state == State.ASK_PLAYER_GAME)
-        server.ask_game.assert_called()
+        self.server.ask_game.assert_called()
