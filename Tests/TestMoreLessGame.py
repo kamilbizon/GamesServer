886ffe6448fc671ea0d30387bef36168360b0637
kamilbizon <kamilbizon@outlook.com>
2018-05-14 12:42:42 +0100

    test ml set range

--- Tests/TestMoreLessGame.py
+++ Tests/TestMoreLessGame.py
@@ -1,8 +1,9 @@
 import unittest
+from unittest.mock import Mock
 from server.morelessGame.MoreLessGame import MoreLessGame
 
 
-class TemporaryClass:
+class TemporaryTCPClass:
     def get_player_list(self):
         pass
 
@@ -13,30 +14,87 @@ def pass_function():
 
 class TestMoreLessGame(unittest.TestCase):
 
-    temp = TemporaryClass()
+    def test_set_min_range(self):
+        temp = TemporaryTCPClass()
+        more_less_game = MoreLessGame(temp)
 
-    more_less_game = MoreLessGame(temp)
-    more_less_game._min_range = 0
-    more_less_game._max_range = 10
-    more_less_game._num_to_guess = 5
+        class TemporaryClassForSetMinRangeTest:
+            first_call = True
 
-    more_less_game._TCP.more = pass_function
-    more_less_game._TCP.less = pass_function
+            def get_min_range_return_first_wrong_next_correct_value(self):
+                if self.first_call:
+                    self.first_call = False
+                    return None
+                else:
+                    return 0
+
+        tmp = TemporaryClassForSetMinRangeTest()
+
+        more_less_game._TCP.ask_min_range = pass_function
+        more_less_game._TCP.get_min_range = tmp.get_min_range_return_first_wrong_next_correct_value
+        more_less_game._TCP.wrong_min_range = Mock()
+
+        more_less_game.set_min_range()
+        more_less_game._TCP.wrong_min_range.assert_called_once()
+        self.assertEqual(0, more_less_game._min_range)
+
+    def test_set_max_range(self):
+        temp = TemporaryTCPClass()
+        more_less_game = MoreLessGame(temp)
+
+        class TemporaryClassForSetMaxRangeTest:
+            first_call = True
+
+            def get_max_range_return_first_wrong_next_correct_value(self, min_range):
+                if self.first_call:
+                    self.first_call = False
+                    return None
+                else:
+                    return 10
+
+        tmp = TemporaryClassForSetMaxRangeTest()
+
+        more_less_game._min_range = 0
+        more_less_game._TCP.ask_max_range = pass_function
+        more_less_game._TCP.get_max_range = tmp.get_max_range_return_first_wrong_next_correct_value
+        more_less_game._TCP.wrong_max_range = Mock()
+
+        more_less_game.set_max_range()
+        more_less_game._TCP.wrong_max_range.assert_called_once()
+        self.assertEqual(10, more_less_game._max_range)
 
     def test_player_guess_return_false_for_wrong_guess(self):
+        temp = TemporaryTCPClass()
+        more_less_game = MoreLessGame(temp)
+
+        more_less_game._min_range = 2
+        more_less_game._max_range = 10
+        more_less_game._num_to_guess = 4
+
+        more_less_game._TCP.more = pass_function
+        more_less_game._TCP.less = pass_function
 
         def get_player_guess_return_wrong_value():
             return 6
 
-        self.more_less_game.get_player_guess = get_player_guess_return_wrong_value
-
-        self.assertFalse(self.more_less_game.player_guess())
+        more_less_game.get_player_guess = get_player_guess_return_wrong_value
+        self.assertFalse(more_less_game.player_guess())
 
     def test_player_guess_return_true_for_correct_guess(self):
+        temp = TemporaryTCPClass()
+        more_less_game = MoreLessGame(temp)
+
+        more_less_game._min_range = 2
+        more_less_game._max_range = 10
+        more_less_game._num_to_guess = 4
+
+        more_less_game._TCP.congratulate_win = pass_function
 
         def get_player_guess_return_correct_value():
             return 4
 
-        self.more_less_game.get_player_guess = get_player_guess_return_correct_value
+        more_less_game.get_player_guess = get_player_guess_return_correct_value
 
-        self.assertFalse(self.more_less_game.player_guess())
+        self.assertTrue(more_less_game.player_guess())
+
+
